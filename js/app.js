038ca2767a12ec1922a94002078609fa8685b111
somethingsaid <kwong.billy@gmail.com>
2016-03-12 15:16:40 -0500

    Calls and URL-piecing all work.  Now to make pretty.

--- js/app.js
+++ js/app.js
@@ -7,34 +7,45 @@ var inputLocation = "";
 // Functions
 
 function getPhoto(coord) {
-	// insta key: 42711593.81734e3.689a6cf94b2a4fcb926cca41dff033a1
 	// flickr api key:
 	console.log("ready to hit flickr");
 	var parameters = {
 		format: "json",
 		method: "flickr.photos.search",
 		api_key: "05b7506e3fd86ae08a540a59e4e7f40d",
-		//sort: "interestingness_desc", //optional, defaults to date-posted-desc,
-		accuracy: 12, // range [1: 16] where 1 is world, 16 is street, 11 is around city
-		min_taken_date: moment().subtract(6, "months").format('YYYY-MM-DD 00:00:00'),
+		nojsoncallback: 1,  // return raw JSON instead of JSONP
+        lat : coord[0],
+		lon : coord[1],
+		/* optional parameters: */
+		content_type: 1, // 1 for photos only
+		sort: "relevance", //optional
+		accuracy: 13, // range [1: 16] where 1 is world, 16 is street, 11 is around city
+		min_taken_date: moment().subtract(12, "months").format('YYYY-MM-DD 00:00:00'),
 		max_upload_date: moment().format('YYYY-MM-DD 00:00:00'),
-		lat : coord[0], // e.g. -23.8634189
-		lon : coord[1], // e.g. -69.1328491
 		radius: 10, // range (0: 32] km
 		radius_units: "km",
-		per_page: 5, // defaults to 100, max 500 per page
-		extras: "url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o"
+		per_page: 30, // defaults to 100, max 500 per page
+		extras: "url_t, url_s, url_m, url_z"
 	};
     
 	$.ajax({
     	url:  "https://api.flickr.com/services/rest/",
     	type: "GET",
     	data: parameters
+    })
+    .done(function(result){
+    	console.log(result);
+    	//https://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
+    	$.each(result.photos.photo, function(i, photoObj)
+    		// photo is an array of objects, feed index and matching object
+    	{
+    		console.log(photoObj);
+    		console.log("Title: " + photoObj.title);
+    		console.log("Source URL: " + photoObj.url_m);
+    		console.log("Link to flickr: https://www.flickr.com/photos/" + photoObj.owner + "/" + photoObj.id);
+    		console.log("*******************************");
+    	});
     });
-}
-
-var jsonFlickrAPI = function(rsp){
-	console.log(rsp);
 }
 
 function getGeocode(location){
