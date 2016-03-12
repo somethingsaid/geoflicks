337f7a3b87b506038709f71b71d491fcb2f74bf5
somethingsaid <kwong.billy@gmail.com>
2016-03-12 14:17:15 -0500

    Can't seem to get flickr jsonp response to display in console

--- js/app.js
+++ js/app.js
@@ -5,31 +5,36 @@ console.log("6 months ago: " + moment().subtract(6, "months").format('YYYY-MM-DD
 var inputLocation = "";
 
 // Functions
+
 function getPhoto(coord) {
 	// insta key: 42711593.81734e3.689a6cf94b2a4fcb926cca41dff033a1
 	// flickr api key:
+	console.log("ready to hit flickr");
 	var parameters = {
+		format: "json",
 		method: "flickr.photos.search",
 		api_key: "05b7506e3fd86ae08a540a59e4e7f40d",
-		sort: "interestingness_desc", //optional, defaults to date-posted-desc,
+		//sort: "interestingness_desc", //optional, defaults to date-posted-desc,
 		accuracy: 12, // range [1: 16] where 1 is world, 16 is street, 11 is around city
 		min_taken_date: moment().subtract(6, "months").format('YYYY-MM-DD 00:00:00'),
-		max_taken_date: moment().format('YYYY-MM-DD 00:00:00'),
-		lat : coord[0], // e.g. 33.7734897
-		lon : coord[1], // e.g. -116.3404429
+		max_upload_date: moment().format('YYYY-MM-DD 00:00:00'),
+		lat : coord[0], // e.g. -23.8634189
+		lon : coord[1], // e.g. -69.1328491
 		radius: 10, // range (0: 32] km
 		radius_units: "km",
+		per_page: 5, // defaults to 100, max 500 per page
+		extras: "url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o"
 	};
-
-    $.ajax({
+    
+	$.ajax({
     	url:  "https://api.flickr.com/services/rest/",
     	type: "GET",
-    	data: parameters,
-    	dataType: "jsonp"
-    })
-    .done(function(result){
-	console.log(result);
+    	data: parameters
     });
+}
+
+var jsonFlickrAPI = function(rsp){
+	console.log(rsp);
 }
 
 function getGeocode(location){
@@ -49,6 +54,7 @@ function getGeocode(location){
     	geoCoordinates.push(result.results[0].geometry.location.lat);
     	geoCoordinates.push(result.results[0].geometry.location.lng);
     	console.log(location + ": " + geoCoordinates);
+    	getPhoto(geoCoordinates);
     })
     .fail(function(jqXHR, error){
     	console.log("call failed");
@@ -62,5 +68,5 @@ $(".location-getter").submit(function(event){
 	inputLocation = $(this).find("input[name='location']").val();
 	console.log("user entered: " + inputLocation);
 	getGeocode(inputLocation);
-})
+    });
 });
