9e3b67702d7add4ac58e789f94ccf3079957df8c
somethingsaid <kwong.billy@gmail.com>
2016-03-17 14:33:54 -0400

    Re-ordered app.js contents

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-console.log("Current date: " + moment().format('YYYY-MM-DD 00:00:00'));
-console.log("6 months ago: " + moment().subtract(6, "months").format('YYYY-MM-DD 00:00:00'));
-
 // Global variables
 var inputLocation = "";
 var photoAlbum = [];
@@ -12,9 +8,23 @@ var geoCoordinates = [];
 var randList = ["Labuan Bajo, Komodo", "New York City, New York", "Paris, France", "Halstatt, Austria", "Sucre, Bolivia", "Bhutan", "Otaru, Hokkaido"];
 var googleApiKey = "AIzaSyBbLnfemMfCf7sJ83aiYAzb8-HR7nJAoOE";
 var flickrApiKey = "05b7506e3fd86ae08a540a59e4e7f40d";
-var xhr = new XMLHttpRequest();
 
 // Functions
+function initMap() {
+            var mapProp = {
+                center: new google.maps.LatLng(51.508742, -0.120850),
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById('map'), mapProp);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.src = "http://maps.googleapis.com/maps/api/js?key=" + googleApiKey + "&callback=initMap";
+  document.body.appendChild(script);
+}
+
 /*  Can't get while loop working to keep randomizing if album is empty
 function randCoordinates() {
     randLat = Math.floor(Math.random() * (66.6 - (-33.3) + 1.0000)) + (-66.6); // +- world's pop distr. in 2000 by lat
@@ -25,25 +35,21 @@ function randCoordinates() {
 */
 
 function showPhoto(album){
-	var albumIndex = 0;
-	var htmlString = "";
-	while(albumIndex < album.length) {
+    var albumIndex = 0;
+    var htmlString = "";
+    while(albumIndex < album.length) {
         for(var row = 1; row < 5; row++){
-        	if(albumIndex < album.length){
-    	        htmlString += "<div class='row'>";
-    	        for(var col = 1; col < 5; col++){
-    	        	if(albumIndex < album.length){
-    	        		/*
-    		            var imgText = "<a target='_blank' href='" + album[albumIndex].flickrPage + "'>    </a>"; // <img src='" + album[albumIndex].url + "'>
-    		            htmlString += "<div class='col-md-3' style='background-image:url(\"" + album[albumIndex].url + "\");background-size:cover;background-repeat:no-repeat;'>" + imgText + "</div>";
-    		            */
-    		            var imgText = "<a target='_blank' href='" + album[albumIndex].flickrPage + "'><img class='img-thumbnail' src='" + album[albumIndex].url + "' width='280' height='190'></a>";
-    		            htmlString += "<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>" + imgText + "</div>";
-    		            albumIndex += 1;
-    	            };
-    	        };
-    	    };
-    	    htmlString += "</div>";
+            if(albumIndex < album.length){
+                htmlString += "<div class='row'>";
+                for(var col = 1; col < 5; col++){
+                    if(albumIndex < album.length){
+                        var imgText = "<a target='_blank' href='" + album[albumIndex].flickrPage + "'><img class='img-thumbnail' src='" + album[albumIndex].url + "' width='280' height='190'></a>";
+                        htmlString += "<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>" + imgText + "</div>";
+                        albumIndex += 1;
+                    };
+                };
+            };
+            htmlString += "</div>";
         };
     };
     $("#album").append(htmlString);
@@ -53,91 +59,101 @@ function showPhoto(album){
 }
 
 function getPhoto(coord){
-	// flickr api key:
-	console.log("ready to hit flickr");
-	var parameters = {
-		format: "json",
-		method: "flickr.photos.search",
-		api_key: flickrApiKey,
-		nojsoncallback: 1,  // return raw JSON instead of JSONP
+    // flickr api key:
+    console.log("ready to hit flickr");
+    var parameters = {
+        format: "json",
+        method: "flickr.photos.search",
+        api_key: flickrApiKey,
+        nojsoncallback: 1,  // return raw JSON instead of JSONP
         lat : coord[0],
-		lon : coord[1],
-		/* optional parameters: */
-		content_type: 1, // 1 for photos only
-		sort: "interestingness_desc", //can also be interestingness_desc, time based
-		accuracy: accuracy, // range [1: 16] where 1 is world, 16 is street, 11 is around city
-		min_taken_date: moment().subtract(12, "months").format('YYYY-MM-DD 00:00:00'),
-		max_upload_date: moment().format('YYYY-MM-DD 00:00:00'),
-		radius: radius, // range (0: 32] km
-		radius_units: "km",
-		per_page: 16, // defaults to 100, max 500 per page
-		page: flickrPage,
-		extras: "url_t, url_s, url_m, url_z"
-	};
+        lon : coord[1],
+        /* optional parameters: */
+        content_type: 1, // 1 for photos only
+        sort: "interestingness_desc", //can also be interestingness_desc, time based
+        accuracy: accuracy, // range [1: 16] where 1 is world, 16 is street, 11 is around city
+        min_taken_date: moment().subtract(12, "months").format('YYYY-MM-DD 00:00:00'),
+        max_upload_date: moment().format('YYYY-MM-DD 00:00:00'),
+        radius: radius, // range (0: 32] km
+        radius_units: "km",
+        per_page: 16, // defaults to 100, max 500 per page
+        page: flickrPage,
+        extras: "url_t, url_s, url_m, url_z"
+    };
     
-	$.ajax({
-    	url:  "https://api.flickr.com/services/rest/",
-    	type: "GET",
-    	data: parameters
+    $.ajax({
+        url:  "https://api.flickr.com/services/rest/",
+        type: "GET",
+        data: parameters
     })
     .done(function(result){
-    	console.log(result);
-    	photoAlbum = []; // re-initialize
-    	$.each(result.photos.photo, function(i, photoObj)
-    		// photo is an array of objects, feed index and matching object
-    	{
-    		var photoDetails = {
-    			title: photoObj.title,
-    			url: photoObj.url_m,
-    			flickrPage: "https://www.flickr.com/photos/" + photoObj.owner + "/" + photoObj.id
-    		}
-    		photoAlbum.push(photoDetails);
-    	});
-    	console.log(photoAlbum);
-    	if(photoAlbum.length > 0){
-    	    showPhoto(photoAlbum);
-    	}
-    	else{
-    		$(".loading").hide();
+        console.log(result);
+        photoAlbum = []; // re-initialize
+        $.each(result.photos.photo, function(i, photoObj)
+            // photo is an array of objects, feed index and matching object
+        {
+            var photoDetails = {
+                title: photoObj.title,
+                url: photoObj.url_m,
+                flickrPage: "https://www.flickr.com/photos/" + photoObj.owner + "/" + photoObj.id
+            }
+            photoAlbum.push(photoDetails);
+        });
+        console.log(photoAlbum);
+        if(photoAlbum.length > 0){
+            showPhoto(photoAlbum);
+        }
+        else{
+            $(".loading").hide();
             $(".success").css("color", "red").show();
             $(".fail").show();
-    	}
+        }
     })
 }
 
 function getGeocode(location){
-	var parameters = {
-		key: googleApiKey,
-		address: location
-	};
+    var parameters = {
+        key: googleApiKey,
+        address: location
+    };
 
     $.ajax({
-    	url: "https://maps.googleapis.com/maps/api/geocode/json",
-    	type: "GET",
+        url: "https://maps.googleapis.com/maps/api/geocode/json",
+        type: "GET",
         data: parameters
     })
     .done(function(result){
-    	console.log(result);
-    	if (result.results.length > 0){
+        console.log(result);
+        if (result.results.length > 0){
             geoCoordinates = []; // re-setting coordinates
-    	    geoCoordinates.push(result.results[0].geometry.location.lat);
-    	    geoCoordinates.push(result.results[0].geometry.location.lng);
-    	    console.log(location + ": " + geoCoordinates);
-    	    getPhoto(geoCoordinates);
+            geoCoordinates.push(result.results[0].geometry.location.lat);
+            geoCoordinates.push(result.results[0].geometry.location.lng);
+            console.log(location + ": " + geoCoordinates);
+            getPhoto(geoCoordinates);
             initMap(geoCoordinates);
-    	}
-    	else {
-    		console.log("no results");
-    	}
+        }
+        else {
+            console.log("no results");
+        }
     })
     .fail(function(jqXHR, error){
-    	console.log("call failed");
+        console.log("call failed");
     });
 }
+
+/******************************************************************/
+/* Execute when page finishes loading */
+/******************************************************************/
+$(document).ready(function(){
+console.log("Current date: " + moment().format('YYYY-MM-DD 00:00:00'));
+console.log("6 months ago: " + moment().subtract(6, "months").format('YYYY-MM-DD 00:00:00'));
+
 
 /*************************************
 // Event listeners and actions
 **************************************/
+// Initialize map
+loadScript();
 
 // Get user's location input
 $("#location-getter").submit(function(event){
